648ce405a19ea5ec235d0ad1459818c8b8589deb
Shweta Rajbhar <swetarajbhar3@gmail.com>
2021-02-03 22:16:55 +0530

    First Commit

---
+++ react-counter-app/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import Button from './components/Button'
+export default class App extends Component
+{
+
+  constructor()
+  {
+    super();
+    this.state = {
+      count:0
+    }
+  }
+
+incrementCount = () =>
+{
+  this.setState({
+  count : this.state.count + 1
+  })
+}
+
+decrementCount = () => {
+this.setState({
+count : this.state.count - 1
+  })
+}
+
+render(){
+let { count } = this.state
+
+return(
+<div>
+
+  <h2> Count : { count }</h2>
+
+  <Button title = { "+" }
+   task = { () => this.incrementCount()} >
+   </Button>
+
+  <Button title = { "-" } 
+   task = { () => this.decrementCount()} >
+   </Button>
+</div>
+
+);
+}
+}
